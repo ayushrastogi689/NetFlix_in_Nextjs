41c28053ee05352a1745609462acbe6b99ad40d3
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-17 10:11:43 +0000

    Created a MovieCard component

---
+++ netflix/src/components/MovieCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "@/app/style/common.module.css";
+import Link from "next/link";
+
+const MovieCard = (element) => {
+  const { id, type, title, synopsis } = element.jawSummary;
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.card_image}>
+          <Image
+            src={element?.jawSummary?.backgroundImage?.url}
+            alt="title"
+            width={250}
+            height={200}
+          />
+        </div>
+        <div className={styles.card_data}>
+          <h2>{title}</h2>
+          <p>{synopsis}</p>
+          <Link href={`/movies/${id}`}>
+            <button>Read More</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MovieCard;
