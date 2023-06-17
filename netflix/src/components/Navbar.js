bd8be5128c2320e6cc739274cce845f2317fb10f
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-17 10:02:32 +0000

    Feature - Created Navbar component

---
+++ netflix/src/components/Navbar.js
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "@/app/style/navbar.module.css";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className="">
+        <ul className={styles.navbarList}>
+          <li className={styles.navbarItem}>
+            <Link className={styles.navbarLinks} href={"/"}>
+              Home
+            </Link>
+          </li>
+          <li className={styles.navbarItem}>
+            <Link className={styles.navbarLinks} href={"/movies"}>
+              Movies
+            </Link>
+          </li>
+          <li className={styles.navbarItem}>
+            <Link className={styles.navbarLinks} href={"/about"}>
+              About
+            </Link>
+          </li>
+          <li className={styles.navbarItem}>
+            <Link className={styles.navbarLinks} href={"/contact"}>
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
