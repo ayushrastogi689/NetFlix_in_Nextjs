cd81b5864dc8e89a6972a740b9c6a52eb7a42426
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-19 15:44:01 +0000

    Added Dynamic route for opening individual movies.

--- netflix/src/app/movies/[id]/page.js
+++ netflix/src/app/movies/[id]/page.js
@@ -1,9 +1,41 @@
 import React from "react";
+import styles from "@/app/style/common.module.css";
+import Image from "next/image";
+import Link from "next/link";
+const page = async ({ params }) => {
+  const id = params.id;
+  const url =
+    "https://netflix54.p.rapidapi.com/title/details/?ids=${id}&lang=en";
 
-const page = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "a8bba4f561msh2974ffb3a084a57p1da0bbjsn9edb8337237d",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+  const main_data = data[0].details;
+
   return (
-    <div>
-      <h1>I am dynamic route</h1>
+    <div className={styles.container}>
+      <h2 className={styles.movie_title}>
+        Netflix / <span>{main_data.type}</span>
+      </h2>
+      <div>
+        <Image
+          src={main_data.backgroundImage.url}
+          alt={main_data.title}
+          width={600}
+          height={300}
+        />
+      </div>
+      <div>
+        <h1>{main_data.title}</h1>
+        <p>{main_data.synopsis}</p>
+      </div>
     </div>
   );
 };
