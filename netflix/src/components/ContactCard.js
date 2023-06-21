cd8885934a96636f1a18dcc3939ff6d6df1597db
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-21 09:12:36 +0000

    Created a contact card

--- netflix/src/components/ContactCard.js
+++ netflix/src/components/ContactCard.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "@/app/style/contact.module.css";
-import {MdEmail} from "react-icons/md"
+import { MdEmail } from "react-icons/md";
+import Link from "next/link";
+
 const ContactCard = () => {
   return (
     <div className={styles.section}>
       <div className={styles.container}>
         <div className={styles.grid}>
           <div className={styles.grid_card}>
-          <i><MdEmail /></i>
+            <i>
+              <MdEmail />
+            </i>
             <h2>Email</h2>
+            <h3>Monday to Friday</h3>
+            <p className={styles.last_para}> Expected response time: 48 Hrs</p>
+            <Link href="ayushrastogi689@gmail.com">Send Email<span></span></Link>
           </div>
         </div>
       </div>
