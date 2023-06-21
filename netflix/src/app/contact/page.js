c66d2e28a537b4ac185d269c2e8d6424f2ab148f
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-21 09:01:02 +0000

    Added Contact Card component in contact us page

--- netflix/src/app/contact/page.js
+++ netflix/src/app/contact/page.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import ContactCard from "@/components/ContactCard";
 const Contact = () => {
-  return <div>Contact Us</div>;
+  return (
+    <div>
+      <div>Contact Us</div>
+      <ContactCard />
+    </div>
+  );
 };
 
 export default Contact;
