127581593ae9b65d86dd3a1d58151ebf4def3028
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-06-24 17:03:02 +0000

    Added a send button and created form handling

--- netflix/src/components/ContactForm.js
+++ netflix/src/components/ContactForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "@/app/style/contact.module.css";
 import { Inter, Mulish } from "next/font/google";
 
@@ -11,9 +12,24 @@ const mulish = Mulish({
 });
 
 const ContactForm = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  }
+
+  function handleSubmit() {}
+
   return (
     <div>
-      <form className={styles.contact_form}>
+      <form className={styles.contact_form} onSubmit={handleSubmit}>
         <div className={styles.input_field}>
           <label className={styles.label} htmlFor="username">
             Name:
@@ -23,6 +39,9 @@ const ContactForm = () => {
               name="username"
               id="username"
               placeholder="Enter your Name"
+              value={user.username}
+              onChange={handleChange}
+              autoComplete="off"
             />
           </label>
         </div>
@@ -36,6 +55,9 @@ const ContactForm = () => {
               name="email"
               id="email"
               placeholder="someone@example.com"
+              value={user.email}
+              onChange={handleChange}
+              autoComplete="off"
             />
           </label>
         </div>
@@ -48,8 +70,14 @@ const ContactForm = () => {
               name="message"
               id="message"
               placeholder="Your message here..."
+              value={user.message}
+              onChange={handleChange}
+              autoComplete="off"
             />
           </label>
+        </div>
+        <div className={mulish.className}>
+          <button type="submit">Send</button>
         </div>
       </form>
     </div>
